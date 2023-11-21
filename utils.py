2f01bb66801d501fbc849d13c9a443b934a9a127
kabadiavadhoot2610 <kabadiavadhoot2610@gmail.com>
2023-11-22 02:46:08 +0530

    Assignment 1

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from sklearn import datasets,svm
+from sklearn import datasets,svm,metrics
 from sklearn.model_selection import train_test_split
 
 def read_digits():
@@ -24,7 +24,15 @@ def train_model(X,y,model_params,model_type = 'svm'):
     model.fit(X,y)
     return model
 
-def train_test_dev_split():
-    pass
-def predict_and_eval():
-    pass
+def train_test_dev_split(X, y, test_size, dev_size):
+    X_train_dev, X_test, Y_train_Dev, y_test =  split_data(X, y, test_size=test_size, random_state=1)
+    print("train+dev = {} test = {}".format(len(Y_train_Dev),len(y_test)))
+    
+    X_train, X_dev, y_train, y_dev = split_data(X_train_dev, Y_train_Dev, dev_size/(1-test_size), random_state=1)
+        
+    return X_train, X_test, X_dev, y_train, y_test, y_dev
+
+# Question 2:
+def predict_and_eval(model, X_test, y_test):
+    predicted = model.predict(X_test)
+    return metrics.accuracy_score(y_test, predicted), metrics.f1_score(y_test, predicted, average="macro"), predicted
